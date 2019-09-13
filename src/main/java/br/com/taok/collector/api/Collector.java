e6554766b3a41d6fe6d3b6cd6203fdce8852348e
Mattheus CAssundé <mattheus.089@gmail.com>
2019-09-13 20:00:39 -0300

    feat: new methods

--- src/main/java/br/com/taok/collector/api/Collector.java
+++ src/main/java/br/com/taok/collector/api/Collector.java
@@ -1,11 +1,21 @@
 package br.com.taok.collector.api;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import br.com.taok.collector.api.model.Provider;
 import br.com.taok.collector.api.model.standard.CollectedLaunch;
 import br.com.taok.collector.api.model.standard.Search;
 
+/**
+ * 
+ * Class that will standardize the data collect
+ * 
+ * */
 public interface Collector {
 
-	public List<CollectedLaunch> collectNow(Search search);
+	public Collector collectNow(Search search);
+	public List<CollectedLaunch> launchs();
+	public List<Provider> providers();
+	public BigDecimal amount();
 }
